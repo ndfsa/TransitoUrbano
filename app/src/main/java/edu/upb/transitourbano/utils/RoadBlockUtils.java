08048fd573ad6eaac0ae8c8363aedfd6479ec6f0
Andres <andresduarter13@gmail.com>
2019-12-08 19:05:33 -0400

    List of Bloqued Roads, Discount class, Adapter of Road Block abd road_block_list_item

---
+++ app/src/main/java/edu/upb/transitourbano/utils/RoadBlockUtils.java
@@ -0,0 +1,21 @@
+package edu.upb.transitourbano.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.upb.transitourbano.R;
+import edu.upb.transitourbano.models.Discount;
+import edu.upb.transitourbano.models.RoadBlock;
+import edu.upb.transitourbano.models.User;
+
+public class RoadBlockUtils {
+
+    public static List<RoadBlock> getBlockedRoads(){
+        List<RoadBlock>blockedRoads = new ArrayList<>();
+        blockedRoads.add(new RoadBlock(1,"Here","Is Bloqued", "Andres", 45,89));
+        blockedRoads.add(new RoadBlock(2,"Here","Is Bloqued", "Gabo", 56,89));
+        blockedRoads.add(new RoadBlock(3,"Here","Is Bloqued", "Sergio", 45,84));
+
+        return blockedRoads;
+    }
+}
