1f1d4e5f53f8cef78ee66e0c0030bbbfc0e17587
gabojm <gabojarmuaz@gmail.com>
2019-12-09 18:36:42 -0400

    Login function (hardcoded user@gmail.com 12345)

--- app/src/main/java/edu/upb/transitourbano/viewmodel/LoginViewModel.java
+++ app/src/main/java/edu/upb/transitourbano/viewmodel/LoginViewModel.java
@@ -4,35 +4,9 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 
 public class LoginViewModel extends AndroidViewModel {
     public LoginViewModel(@NonNull Application application) {
         super(application);
     }
-/*
-    public LiveData<Base> login(final String email, final String password) {
-        final MutableLiveData<Base> result = new MutableLiveData<>();
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(1000);
-                    User user = new User();
-                    user.setUuid("1");
-                    user.setEmail(email);
-                    user.setStatus(true);
-
-                    UserLogged userLogged = ResponseMapper.mapUserToUserLooged(user);
-                    result.postValue(new Base(userLogged));
-                } catch (InterruptedException ex) {
-                    result.postValue(new Base("Interrupted", ex));
-                }
-            }
-        }).start();
-        return result;
-    }
- */
-
 }
