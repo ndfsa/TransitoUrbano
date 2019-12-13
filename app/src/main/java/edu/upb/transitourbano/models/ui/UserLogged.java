16b664756147572345d766343d0201c6ffa9c7d5
gabojm <gabojarmuaz@gmail.com>
2019-12-12 20:15:35 -0400

    Login auth

--- app/src/main/java/edu/upb/transitourbano/models/ui/UserLogged.java
+++ app/src/main/java/edu/upb/transitourbano/models/ui/UserLogged.java
@@ -1,4 +1,14 @@
 package edu.upb.transitourbano.models.ui;
 
 public class UserLogged {
+    private String email;
+
+    public UserLogged(String email){
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
 }
