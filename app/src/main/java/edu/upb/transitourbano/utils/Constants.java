117f5b20879adf4252e6d7cf5a45b6fd04e51e23
Andres <andresduarter13@gmail.com>
2019-12-16 21:41:31 -0400

    Map fragment displayed

--- app/src/main/java/edu/upb/transitourbano/utils/Constants.java
+++ app/src/main/java/edu/upb/transitourbano/utils/Constants.java
@@ -1,4 +1,10 @@
 package edu.upb.transitourbano.utils;
 
 public class Constants {
+
+    public static final double DEFAULT_LAT = -16.524747;
+    public static final double DEFAULT_LNG = -68.110466;
+    public static final float DEFAULT_ZOOM = 16;
+
+
 }
