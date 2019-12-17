fab35870aaa038ec215cc588c939c66af23bb78f
Andres <andresduarter13@gmail.com>
2019-12-16 21:41:31 -0400

    Map fragment displayed

--- app/src/main/java/edu/upb/transitourbano/ui/fragments/MapFragment.java
+++ app/src/main/java/edu/upb/transitourbano/ui/fragments/MapFragment.java
@@ -4,15 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import edu.upb.transitourbano.R;
+import edu.upb.transitourbano.utils.Constants;
 
-public class MapFragment extends BaseFragment {
+public class MapFragment extends BaseFragment implements OnMapReadyCallback {
 
-    private TextView textView;
+    private GoogleMap map;
+    private MapView mapView;
 
     public MapFragment() {
         super();
@@ -23,7 +32,20 @@ public class MapFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_map, container, false);
         initUI(view);
+        initMap(savedInstanceState);
         return view;
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        map.getUiSettings().setZoomControlsEnabled(true);
+        //map.getUiSettings().isMyLocationButtonEnabled();
+        //map.setMyLocationEnabled(true);
+        //map.getUiSettings().setZoomGesturesEnabled(true);
+        //map.setPadding(0, 0, 0, mapView.getHeight());
+
+        LatLng maker = new LatLng(Constants.DEFAULT_LAT, Constants.DEFAULT_LNG);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(maker, Constants.DEFAULT_ZOOM));
     }
 
     @Override
@@ -32,6 +54,21 @@ public class MapFragment extends BaseFragment {
     }
 
     private void initUI(View view) {
-        textView = view.findViewById(R.id.mapFragmentTextView);
+        mapView = view.findViewById(R.id.map);
+
     }
+
+    private void initMap(Bundle savedInstanceState) {
+        mapView.onCreate(savedInstanceState);
+        mapView.onResume();
+        try {
+            MapsInitializer.initialize(context);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mapView.getMapAsync(this);
+    }
+
+
+
 }
